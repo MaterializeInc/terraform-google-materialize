514cec2b6b1ef2892bd94425f41843e0dd4cdb39
GitHub <noreply@github.com>
2025-03-25 13:19:49 +0000

    chore: bump terraform-helm-materialize to v0.1.9
    Signed-off-by: GitHub <noreply@github.com>

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ module "storage" {
 }
 
 module "operator" {
-  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.8"
+  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.9"
 
   count = var.install_materialize_operator ? 1 : 0
 
