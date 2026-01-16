d37ce6f623cd715b77bc7c0163b9aa6a39d6a54b
Materialize <infra+bot@materialize.com>
2026-01-16 14:08:06 +0000

    Bump to terraform-helm-materialize v0.1.44

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ module "certificates" {
 }
 
 module "operator" {
-  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.43"
+  source = "github.com/MaterializeInc/terraform-helm-materialize?ref=v0.1.44"
 
   count = var.install_materialize_operator ? 1 : 0
 
